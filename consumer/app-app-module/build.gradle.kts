ad7dd6d8ad9ff6ed2f8c3cc75c7bf01ef03b7f32
julia <julia@polyforest.com>
2019-05-14 15:48:51 -0700

    Update copyrights and some code r

--- consumer/app-app-module/build.gradle.kts
+++ consumer/app-app-module/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Poly Forest
+ * Copyright 2019 Poly Forest, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-import com.polyforest.acornui.build.acornui
 
 plugins {
 	id("com.polyforest.acornui.app")
--- src/main/kotlin/com/polyforest/acornui/builder.gradle.kts
+++ src/main/kotlin/com/polyforest/acornui/builder.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Poly Forest
+ * Copyright 2019 Poly Forest, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
 
 package com.polyforest.acornui
 
-import com.polyforest.acornui.build.*
+import com.polyforest.acornui.build.acornui
 
 /**
  * Plugin:  com.polyforest.acornui.builder
